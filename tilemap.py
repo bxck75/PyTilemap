c3e98dbf74254196b0fe03b1c982fd562553b051
William Muhu <wmuhucraft@gmail.com>
2017-04-01 15:22:46 +0300

    Add some new features

--- tilemap.py
+++ tilemap.py
@@ -23,6 +23,24 @@ class Tilemap:
         self.tilemap = [[texture for e in range(self.width)] for e in range(
         self.height)]
 
+    def fill_row(self, texture, row, freq=0, skip=[]):
+        """Randomly place a texture in a column"""
+        for i in range(0, self.width):
+            if random.randint(0, freq) == freq:
+                if self.tilemap[row][i] in skip:
+                    pass
+                else:
+                    self.tilemap[row][i] = texture
+
+    def fill_col(self, texture, col, freq=0, skip=[]):
+        """Randomly place a texture in a row"""
+        for i in range(0, self.height):
+            if random.randint(0, freq) == freq:
+                if self.tilemap[i][col] in skip:
+                    pass
+                else:
+                    self.tilemap[i][col] = texture
+
     def replace(self, textures, replacement):
         """Replace every instance of a texture with another one"""
         for row in range(self.height):
@@ -30,6 +48,25 @@ class Tilemap:
                 if self.tilemap[row][column] in textures:
                     self.tilemap[row][column] = replacement
 
+    def next_to(self, position, tile):
+        """Check if a tile is next to another one"""
+        try:
+            if self.tilemap[position[0]][position[1]+1] == tile:
+                return True
+        except IndexError: pass
+        try:
+            if self.tilemap[position[0]][position[1]-1] == tile:
+                return True
+        except IndexError: pass
+        try:
+            if self.tilemap[position[0]+1][position[1]] == tile:
+                return True
+        except IndexError: pass
+        try:
+            if self.tilemap[position[0]-1][position[1]] == tile:
+                return True
+        except IndexError: pass
+        return False
     def get_tile(self, position, tiles):
         """Check if a tilemap position contains a certain tile"""
         if self.tilemap[position[0]][position[1]] in tiles:
